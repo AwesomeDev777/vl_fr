8d084aab39cfe96d0b231dde9847d661f5d5a9a0
AwesomeDev777 <bnykyfor@gmail.com>
2023-08-26 19:26:40 +0300

    fixed chart

--- src/modules/apexcharts/index.ts
+++ src/modules/apexcharts/index.ts
@@ -1,5 +1,6 @@
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
+import Chart from "./react-apexcharts"
 
-const Chart = dynamic(() => import('./react-apexcharts'), { ssr: false })
+// const Chart = dynamic(() => import('./react-apexcharts'), { ssr: false })
 
 export default Chart

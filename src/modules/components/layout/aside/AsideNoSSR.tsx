f71a60a6fa13dd0f6270192a4dc49e3aac717113
AwesomeDev777 <bnykyfor@gmail.com>
2023-08-26 18:19:27 +0300

    fix import dynamic

--- src/modules/components/layout/aside/AsideNoSSR.tsx
+++ src/modules/components/layout/aside/AsideNoSSR.tsx
@@ -3,10 +3,11 @@ import AsideAddon from './AsideAddon'
 import AsideBody from './AsideBody'
 import AsideHeader from './AsideHeader'
 import AsideTitle from './AsideTitle'
+import Aside from "./Aside"
 
-const Aside = dynamic(() => import('./Aside'), {
-	ssr: false,
-})
+// const Aside = dynamic(() => import('./Aside'), {
+// 	ssr: false,
+// })
 
 export default Object.assign(Aside, {
 	Body: AsideBody,

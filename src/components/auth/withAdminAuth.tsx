ccb1c5b3d8652d4deebc60c1213e8bfd6643d3df
AwesomeDev777 <bnykyfor@gmail.com>
2023-08-26 17:05:55 +0300

    test for hosting

--- src/components/auth/withAdminAuth.tsx
+++ src/components/auth/withAdminAuth.tsx
@@ -5,7 +5,7 @@ import PAGE from 'config/page.config'
 import type { AdditionalNextPageProps, ExtendedNextPage } from '@blueupcode/components/types'
 import type { NextPageContext } from 'next'
 
-const withAdminAuth = (PageComponent: ExtendedNextPage) => {
+const withAdminAuth = (PageComponent: any) => {
 	// Initialize wrapper component
 	const WrapperComponent: ExtendedNextPage = (props) => {
 		return <PageComponent {...props} />
